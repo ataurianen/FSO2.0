cbe02859a637b45527ac538a075bd2cbca7d3c84
Andrew Taurianen <andrew.taurianen@gmail.com>
2026-02-18 16:40:23 -0800

    Finished 1.2

--- part1/courseinfo/src/App.jsx
+++ part1/courseinfo/src/App.jsx
@@ -11,12 +11,22 @@ const App = () => {
     return <h1>{props.course}</h1>
   }
 
-  const Content = (props) => {
+  const Part = (props) => {
     return (
-      <div>
+      <>
         <p>
           {props.part} {props.exercises}
         </p>
+      </>
+    )
+  }
+
+  const Content = () => {
+    return (
+      <div>
+        <Part part={part1} exercises={exercises1} />
+        <Part part={part2} exercises={exercises2} />
+        <Part part={part3} exercises={exercises3} />
       </div>
     )
   }
@@ -28,9 +38,7 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercises={exercises1} />
-      <Content part={part2} exercises={exercises2} />
-      <Content part={part3} exercises={exercises3} />
+      <Content />
       <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
     </div>
   )

86fbd87560ae4122992634dba2a1ca768b901319
Andrew Taurianen <andrew.taurianen@gmail.com>
2026-02-20 19:11:23 -0800

    Completed 1.6

---
+++ part1/unicafe/src/App.jsx
@@ -0,0 +1,49 @@
+import { useState } from 'react'
+
+const Header = ({ text }) => <h1>{text}</h1>
+
+const Button = (props) => {
+  return (
+    <button onClick={props.handleClick}>{props.text}</button>
+  )
+}
+
+const Stats = ({ text, count}) => {
+  return (
+    <p>{text} {count}</p>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+const handleClickGood = () => {
+  setGood(good + 1)
+}
+
+const handleClickNeutral = () => {
+  setNeutral(neutral + 1)
+}
+
+const handleClickBad = () => {
+  setBad(bad + 1)
+}
+
+  return (
+    <div>
+      <Header text={"Give Feedback"} />
+      <Button handleClick={handleClickGood} text="good" />
+      <Button handleClick={handleClickNeutral} text="neutral" />
+      <Button handleClick={handleClickBad} text="bad" />
+      <Header text={"Statistics"} />
+      <Stats text={"good"} count={good} />
+      <Stats text={"neutral"} count={neutral} />
+      <Stats text={"bad"} count={bad} />
+    </div>
+  )
+}
+
+export default App
